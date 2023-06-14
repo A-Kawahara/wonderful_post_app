e392d1b347ad1ea50ac2a239e615c138a24f36d2
川原杏美 <azu.kawahara@gmail.com>
2023-06-14 21:27:33 +0900

    記事とタグのアソシエーションを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,6 @@ class Article < ApplicationRecord
   validates :title, presence: true, uniqueness: { case_sensitive: false }
   validates :content, presence: true, uniqueness: { case_sensitive: false }
   belongs_to :user
-
+  has_many :tag_articles, dependent: :destroy
+  has_many :tags, through: :tag_articles
 end
